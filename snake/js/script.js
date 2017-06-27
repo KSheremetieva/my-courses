57bf3d2c5617803fff4da700d408f792dbaed689
Ksusha <ksnsher@gmail.com>
2017-06-27 12:26:03 +0300

    add class in css and delete from js

--- snake/js/script.js
+++ snake/js/script.js
@@ -23,18 +23,10 @@ Snake.prototype = {
 			let row = this.arr[i];
 			let inRow = document.createElement('div');
 			inRow.style.height = '13px';
-			this.main.style.display = 'inline-block';
-			// this.main.style.margin = '0 auto'; //не работает почему-то
-			this.main.style.marginLeft = '20%';
-			this.main.style.boxShadow = '0px 0px 210px 13px rgba(61,71,80,1)';
 			for(let j = 0; j < row.length; j++){
 				// console.log(this.main);
 				let div = document.createElement('div');
 				div.className = 'cell';
-				div.style.width = '13px';
-				div.style.height = '100%';
-				div.style.display = 'inline-block';
-				div.style.border = '1px solid rgba(44,44,44,0.1)';
 				if(this.arr[i][j] == 0){
 					div.style.backgroundColor = 'white';
 				}else if(this.arr[i][j] == 1){
