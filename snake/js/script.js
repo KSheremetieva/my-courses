b8e23ae8d1d19658c7e5578715c141d564efa33d
Ksusha <ksnsher@gmail.com>
2017-07-05 09:58:11 +0300

    start creating snake, move one box

--- snake/js/script.js
+++ snake/js/script.js
@@ -1,17 +1,23 @@
 "use strict";
 
 (function(){
-function Snake(config){
+function MainField(config){
 	this.width = config.width;
 	this.height = config.height;
 	this.arr = config.arr;
 	this.cells = config.cells;
 	this.main = document.querySelector('.main');
+	this.length = config.length;
+	this.snake = config.snake;
+	this.direction = config.direction;
+	this._createArr();
+	this._drawArr();
+	this._moveSnake(this.direction, this.snake);
 };
 
-Snake.prototype = {
-	constructor: Snake,
-	createArr: function(){
+MainField.prototype = {
+	constructor: MainField,
+	_createArr: function(){
 		for(let i = 0; i < this.width; i++){
 			this.arr[i] = [];
 			let row = this.arr[i];
@@ -21,27 +27,52 @@ Snake.prototype = {
 		}
 		return this.arr;
 	},
-	drawArr: function(){
+	_drawArr: function(){
 		for(let i = 0; i < this.arr.length; i++){
-			let row = this.arr[i];
-			let inRow = document.createElement('div');
-			// inRow.style.height = '13px';
-			for(let j = 0; j < row.length; j++){
-				// console.log(this.main);
+			for(let j = 0; j < this.arr.length; j++){ // row.length
 				let div = document.createElement('div');
-				div.className = 'cell';
-				if(this.arr[i][j] == 0){
-					div.style.backgroundColor = 'white';
-				}else if(this.arr[i][j] == 1){
-					div.style.backgroundColor = '#0700C5';
-				}else{
-					div.style.backgroundColor = '#FF0000';
-				}
-				// inRow.appendChild(div);
-				this.main.appendChild(div)
+				div.className = 'cell whiteBox';
+				div.setAttribute('id', this._generateId(j, i));
+				this.main.appendChild(div);
+				// console.log(div);
 			}
-			// this.main.appendChild(div)
 		}
+		// let d = document.getElementById(this._generateId(0, 0));
+		// d.className += " greenBox";
+	},
+	_generateId: function(i, j){
+		return "x" + i + "y" + j;
+	},
+	_moveSnake: function(direction, snakePos){
+		let _this = this;
+		let snakeX = snakePos.x;
+		let snakeY = snakePos.y;
+		let d = document.getElementById(this._generateId(snakeX, snakeY));
+		let timer = setInterval(function(){
+			// d.className += " greenBox";
+
+			if(direction == 'down'){
+				// if(snakeY == 0){
+				// 	d.className = d.className.replace(' greenBox', ' whiteBox');	
+				// }
+				// d.className = d.className.replace(' greenBox', ' whiteBox');
+
+				let w = document.getElementById(_this._generateId(snakeX, snakeY));
+				w.className += ' whiteBox';
+				w.className = w.className.replace(' greenBox', ' whiteBox');
+
+				snakeY++;
+				let s = document.getElementById(_this._generateId(snakeX, snakeY));
+				s.className += " greenBox";
+				console.log(s)
+			}
+			console.log(d)
+		}, 1000)
+
+		// switch (this.direction){
+		// 	case 'down': 
+
+		// }
 	}
 };
  
@@ -50,28 +81,48 @@ const config = {
 	height: 50,
 	arr: [],
 	cells: [],
-	main: document.querySelector('.main')
+	main: document.querySelector('.main'),
+	length: 1,
+	snake: {x:0, y:0},
+	direction: 'down'
 };
+
+
+
+function SnakeGame(arg1, arg2){
+	MainField.prototype.constructor.bind(this, arguments);
+	this.name = arg1;
+};
+
+SnakeGame.prototype = Object.create(MainField.prototype);
+SnakeGame.prototype.check = function(){
+	console.log(this.snake);
+}
+
+
 
 document.addEventListener("DOMContentLoaded", ready);
 function ready(){
 	let button = document.getElementById('button');
-	console.log(button);
+	// console.log(button);
 	button.addEventListener('click', start);
 };
 
-// var button = document.querySelector('.button'); // почему не работает??
-
-// var but = document.getElementById('button'); //не работает
-
 function start(){
 	let button = document.getElementById('button');
-	console.log(button);
+	// console.log(button);
 	button.style.display = 'none';
 	let main = document.querySelector('main');
 	main.style.display ="block";
-	var snake = new Snake(config);
-	snake.createArr();
-	snake.drawArr();
-}
+	var mainField = new MainField(config);
+	// mainField._drawArr();
+	// var snakeGame = new SnakeGame('snake');
+	// snakeGame.check();
+	// snakeGame._drawArr();
+	// document.addEventListener('keydown', (e)=>{
+		// mainField._moveSnake(e);
+		// snakeGame._moveSnake(e)
+	// });
+};
+
 }());
