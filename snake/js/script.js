92585d122c48e4b5181a4a387660f83f556e6b99
Ksusha <ksnsher@gmail.com>
2017-07-12 17:52:51 +0300

    create new method to move snake

--- snake/js/script.js
+++ snake/js/script.js
@@ -10,9 +10,10 @@ function MainField(config){
 	this.length = config.length;
 	this.snake = config.snake;
 	this.direction = config.direction;
+	this.direct = config.direct;
 	this._createArr();
-	this._drawArr();
-	this._moveSnake();
+	this._drawMap();
+	this._drawSnake(this.snake);
 };
 
 MainField.prototype = {
@@ -27,72 +28,45 @@ MainField.prototype = {
 		}
 		return this.arr;
 	},
-	_drawArr: function(){
+	_drawMap: function(){
 		for(let i = 0; i < this.arr.length; i++){
 			for(let j = 0; j < this.arr.length; j++){ // row.length
 				let div = document.createElement('div');
-				div.className = 'cell whiteBox';
+				div.className = 'cell'; //whiteBox
 				div.setAttribute('id', this._generateId(j, i));
 				this.main.appendChild(div);
+			}
+		}
+	},
+	_drawSnake: function(arg){
+		let s = arg;
+		console.log(s)
+		for(let i = 0; i < s.length; i++){
+			let a = this._containsCl(s[i].x, s[i].y);
+			if(!a){
+
+				console.log('tut')
+				this._toggleGreen(s[i].x, s[i].y);
 			}
 		}
 	},
 	_generateId: function(i, j){
 		return "x" + i + "y" + j;
 	},
-	_moveSnake: function(direction, snakePos){
-		let _this = this;
-		setInterval(function(){
-			let snakeX = _this.snake.x;
-			let snakeY = _this.snake.y;
-			if(_this.direction == 'down'){
-				//Clean before
-				let snakeY_bef = _this.snake.y - 1;
-				if(snakeY_bef >= 0){
-				_this._toggle(snakeX, snakeY_bef);
-				};
-				_this._toggle(snakeX, snakeY);
-				_this.snake.y++;
-			};
-
-			if(_this.direction == 'up'){
-				//Clean after
-				let snakeY_aft = _this.snake.y +1;
-				if(snakeY_aft <= 49){
-				_this._toggle(snakeX, snakeY_aft);
-				};
-				_this._toggle(snakeX, snakeY);
-				_this.snake.y--;
-			};
-			
-			if(_this.direction == 'right'){
-				//Clean before
-				let snakeX_bef = _this.snake.x -1;
-				if(snakeX_bef >= 0){
-					_this._toggle(snakeX_bef, snakeY);
-				};
-				_this._toggle(snakeX, snakeY);
-				_this.snake.x++;
-			};
-			
-			if(_this.direction == 'left'){
-				//Clean after
-				let snakeX_aft = _this.snake.x +1;
-				if(snakeX_aft <= 49){
-					_this._toggle(snakeX_aft, snakeY);
-				};
-				_this._toggle(snakeX, snakeY);
-				_this.snake.x--;
-			};
-
-		},1000);
+	_toggleGreen: function(x, y){
+		let elem = document.getElementById(this._generateId(x, y));
+		elem.classList.toggle('greenBox');
 	},
-	_toggle: function(i, y){
-		console.log(i,y)
-		let x = document.getElementById(this._generateId(i, y));
-		x.classList.toggle('greenBox');
+	_toggleWhite: function(x, y){
+		let elem = document.getElementById(this._generateId(x, y));
+		elem.classList.toggle('whiteBox');
+	},
+	_containsCl: function(x, y){
+		let elem = document.getElementById(this._generateId(x, y));
+		return elem.classList.contains('greenBox');
 	}
 };
+//check next step (край карты или яблоко)
  
 const config = {
 	width: 50,
@@ -101,29 +75,101 @@ const config = {
 	cells: [],
 	main: document.querySelector('.main'),
 	length: 1,
-	snake: {x:25, y:25},
+	snake: [{x:25, y:27}, {x:25, y:26}, {x:25, y:25}, {x:25, y:24}, {x:25, y:23}], //{x:25, y:26}, {x:25, y:27}
 	apple: {x:15, y:27},
-	direction: 'left'
+	direction: 'up',
+	direct: {before: 'left', now: 'right'}
 };
+
 
 function SnakeGame(){
 	MainField.prototype.constructor.apply(this, arguments);
 };
 
 SnakeGame.prototype = Object.create(MainField.prototype);
+SnakeGame.prototype._snakeDirection = function(e){
+	switch (e.keyCode){
+		case (38):
+			this.direction = 'up';
+			break;
+		case (40):
+			this.direction = 'down';
+			break;
+		case (39):
+			this.direction = 'right';
+			break;
+		case (37):
+			this.direction = 'left';
+			break;
+	};
+};
+SnakeGame.prototype._moveSnake = function(){
+	let _this = this;
+	setInterval(function(){
+		let snakeLeng = _this.snake.length-1;
+		let snakeX = _this.snake[snakeLeng].x;
+		let snakeY = _this.snake[snakeLeng].y;
+
+		switch (_this.direction){
+			case ('up'):
+				// Добавляем новый элемент с движением вверх
+				--snakeY;
+				_this.snake.push({x: snakeX, y: snakeY});
+				// Убираем цвет у удаленного элемента массива snake
+				let up = _this.snake.shift();
+				_this._toggleGreen(up.x, up.y );
+				// Отрисовка массива с новыми координатами змейки
+				_this._drawSnake(_this.snake);
+				break;
+
+			case ('down'):
+				++snakeY;
+				_this.snake.push({x: snakeX, y: snakeY});
+				let down = _this.snake.shift();
+				_this._toggleGreen(down.x, down.y);
+				_this._drawSnake(_this.snake);
+				break;
+
+			case ('left'):
+				--snakeX;
+				_this.snake.push({x: snakeX, y: snakeY});
+				let left = _this.snake.shift();
+				_this._toggleGreen(left.x, left.y);
+				_this._drawSnake(_this.snake);
+				break;
+
+			case ('right'):
+				++snakeX;
+				_this.snake.push({x: snakeX, y: snakeY});
+				let right = _this.snake.shift();
+				_this._toggleGreen(right.x, right.y);
+				_this._drawSnake(_this.snake);
+
+		}
+	},1000);
+};
+
+
 
 document.addEventListener("DOMContentLoaded", ready);
 function ready(){
 	let button = document.getElementById('button');
 	button.addEventListener('click', start);
 };
-
+var snakeGame;
 function start(){
 	let button = document.getElementById('button');
 	button.style.display = 'none';
 	let main = document.querySelector('main');
 	main.style.display ="block";
-	var snakeGame = new SnakeGame(config);
+	snakeGame = new SnakeGame(config);
+	snakeGame._moveSnake();
+	//direction
+	document.addEventListener('keydown', direction);
 };
+
+function direction(e){
+	snakeGame._snakeDirection(e);
+}
 
 }());
